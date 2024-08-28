674cb1bc0ff643b0818cb17dec52fc994cdf8588
rahul nayak <rahul.nayak@inventyv-git.com>
2024-08-28 19:02:48 +0530

    committed by code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{path::Path, process::Command};
-fn git_push(repo_path: &str, remote: &str, branch: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+fn git_status(repo_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let repo_path = Path::new(repo_path);
 
     if !repo_path.exists() || !repo_path.is_dir() {
@@ -14,30 +15,54 @@ fn git_push(repo_path: &str, remote: &str, branch: &str) -> Result<(), Box<dyn s
                 println!("No changes to push.");
                 return Ok(());
             }
+            println!("Git Status::>{:?}", status_output);
+            Ok(())
         }
         Err(error) => {
             eprint!("{}", error);
+            Err(format!("{}", error).into())
         }
     }
+}
+fn git_pull(repo_path: &str, remote: &str, branch: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let repo_path = Path::new(repo_path);
+
+    if !repo_path.exists() || !repo_path.is_dir() {
+        return Err(format!("Invalid repository path: {}", repo_path.display()).into());
+    }
+    let mut binding = Command::new("git");
+    let command = binding.current_dir(repo_path);
+    let pull_op = command.args(&["pull", "-v", remote, branch]).output();
+    match pull_op {
+        Ok(op) => {
+            println!("op===>{:?}", op);
+        }
+        Err(error) => {
+            eprintln!("{}", error);
+        }
+    }
+    Ok(())
+}
+fn git_push(repo_path: &str, remote: &str, branch: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let repo_path = Path::new(repo_path);
+
+    if !repo_path.exists() || !repo_path.is_dir() {
+        return Err(format!("Invalid repository path: {}", repo_path.display()).into());
+    }
+    let mut binding = Command::new("git");
+    let command = binding.current_dir(repo_path);
 
     println!("Changes to be pushed:");
-    let diff_output = Command::new("git")
-        .current_dir(repo_path)
-        .args(&["add", "./*"])
-        .output();
+    let diff_output =  Command::new("git").current_dir(repo_path).args(&["add", "./*"]).output();
     match diff_output {
-        Ok(_op) => {
-            let commit = Command::new("git")
-                .current_dir(repo_path)
-                .args(&["commit", "-m", "committed by code"])
+        Ok(op) => {
+            println!("Git OP::>{:?}", op);
+            let commit = command.args(&["commit", "-m", "committed by code"])
                 .output();
             match commit {
                 Ok(commit_op) => {
-                    println!("commit status:{}", commit_op.status);
-                    let push_output = Command::new("git")
-                        .current_dir(repo_path)
-                        .args(&["push", "-v", remote, branch])
-                        .output()?;
+                    println!("Git commit :{:?}", commit_op);
+                    let push_output =  Command::new("git").current_dir(repo_path).args(&["push", "-v", remote, branch]).output()?;
 
                     println!("Git push output:");
                     println!("stdout: {}", String::from_utf8_lossy(&push_output.stdout));
@@ -65,7 +90,6 @@ fn git_push(repo_path: &str, remote: &str, branch: &str) -> Result<(), Box<dyn s
             Err(format!("37:::>{}", error).into())
         }
     }
-    // Perform the push
 }
 fn main() {
     // let repo_path = "/home/rahul/Desktop/Node_code/git_wrapper";
@@ -73,6 +97,8 @@ fn main() {
     let remote = "origin";
     let branch = "main";
 
-    let op = git_push(repo_path, remote, branch);
+    let op = git_push(repo_path, &remote, &branch);
+    // let op = git_pull(repo_path, &remote, &branch);
+    // let op = git_status(repo_path);
     println!("{:?}", op);
 }
